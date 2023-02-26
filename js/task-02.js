734c3a74ffa6fc8c742e53a19dd4ba3e108ece5e
Lisiuk-Ruslan <114481433+Lisiuk-Ruslan@users.noreply.github.com>
2023-02-26 10:37:50 +0200

    Правки в завданні 2

--- js/task-02.js
+++ js/task-02.js
@@ -30,5 +30,12 @@ const ingredients = [
 ];
 const ingredientsList = document.querySelector('#ingredients');
 
-const listItem = ingredients.map(ingridient => `<li class ="item">${ingridient}</li>`).join('');
-ingredientsList.innerHTML = listItem;
+// const listItem = ingredients.map(ingridient => `<li class ="item">${ingridient}</li>`).join('');
+// ingredientsList.innerHTML = listItem;
+
+ingredients.forEach(item => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.innerHTML = item;
+  ingredientsItem.classList.add('item');
+  ingredientsList.append(ingredientsItem);
+})
